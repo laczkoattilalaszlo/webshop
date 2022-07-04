220e35e75756b407da2e97d3b4133955368c4a7a
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-04 21:43:04 +0200

    fix: remove some columns () of user table

--- database/schema_generator.sql
+++ database/schema_generator.sql
@@ -1,91 +1,87 @@
 -- RUN THIS FILE TO CREATE THE SUITABLE SQL SCHEMA FOR THE APPLICATION
 
 -- 1. CREATE USER TABLE
-    create table "user"
-    (
-        id                  uuid not null
-                            constraint user_pk
-                            primary key,
-        email               text,
-        password            text,
-        name                text,
-        picture             text,
-        phone               text,
-        cart_id             uuid,
-        shipping_address_id uuid,
-        billing_address_id  uuid,
-        bank_card_id        uuid
-    );
+create table "user"
+(
+    id                  uuid not null
+        constraint user_pk
+            primary key,
+    email               text,
+    password            text,
+    name                text,
+    phone               text,
+    picture             text
+);
 
-    create unique index user_id_uindex
-        on "user" (id);
+create unique index user_id_uindex
+    on "user" (id);
 
 -- 2.  CREATE PRODUCT TABLE
-    create table product
-    (
-        id          uuid not null
-                    constraint product_pk
-                    primary key,
-        description text,
-        price       numeric,
-        currency    text,
-        supplier    text,
-        category    text,
-        picture     text
-    );
+create table product
+(
+    id          uuid not null
+        constraint product_pk
+            primary key,
+    description text,
+    price       numeric,
+    currency    text,
+    supplier    text,
+    category    text,
+    picture     text
+);
 
-    create unique index product_id_uindex
-        on product (id);
+create unique index product_id_uindex
+    on product (id);
 
 -- 3. CREATE CART TABLE
-    create table cart
-    (
-        product_id uuid
-                   constraint table_name_product_id_fk
-                   references product
-                   on delete cascade,
-        quantity   int,
-        user_id    uuid
-                   constraint table_name_user_id_fk
-                   references "user"
-                   on delete cascade
-    );
+create table cart
+(
+    product_id uuid
+        constraint table_name_product_id_fk
+            references product
+            on delete cascade,
+    quantity   int,
+    user_id    uuid
+        constraint table_name_user_id_fk
+            references "user"
+            on delete cascade
+);
 
 -- 4. CREATE SHIPPING ADDRESS TABLE
-    create table shipping_address
-    (
-        zip     int,
-        country text,
-        city    text,
-        address text,
-        user_id uuid
-                constraint shipping_address_user_id_fk
-                references "user"
-                on delete cascade
-    );
+create table shipping_address
+(
+    zip     int,
+    country text,
+    city    text,
+    address text,
+    user_id uuid
+        constraint shipping_address_user_id_fk
+            references "user"
+            on delete cascade
+);
 
 -- 5. CREATE BILLING ADDRESS TABLE
-    create table billing_address
-    (
-        zip     int,
-        country text,
-        city    text,
-        address text,
-        user_id uuid
-                constraint billing_address_user_id_fk
-                references "user"
-                on delete cascade
-    );
+create table billing_address
+(
+    zip     int,
+    country text,
+    city    text,
+    address text,
+    user_id uuid
+        constraint billing_address_user_id_fk
+            references "user"
+            on delete cascade
+);
 
 -- 6. CREATE BANK CARD TABLE
-    create table bank_card
-    (
-        name            text,
-        card_number     text,
-        expiration_date date,
-        security_code   int,
-        user_id         uuid
-                        constraint bank_cart_user_id_fk
-                        references "user"
-                        on delete cascade
-    );
+create table bank_card
+(
+    name            text,
+    card_number     text,
+    expiration_date date,
+    security_code   int,
+    user_id         uuid
+        constraint bank_cart_user_id_fk
+            references "user"
+            on delete cascade
+);
