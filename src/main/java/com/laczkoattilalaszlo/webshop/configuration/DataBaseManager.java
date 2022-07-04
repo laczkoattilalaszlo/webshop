b5f62b0ab23828aa7ba9d5a002422ba6e6e71dab
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-04 23:54:49 +0200

    feat: setup database connection

---
+++ src/main/java/com/laczkoattilalaszlo/webshop/configuration/DataBaseManager.java
@@ -0,0 +1,33 @@
+package com.laczkoattilalaszlo.webshop.configuration;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DataBaseManager {
+
+    // Constructor(s)
+    public DataBaseManager() {
+    }
+
+    // Method(s)
+    public DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+        String dbName = System.getenv().get("DB_NAME");
+        String dbUserName = System.getenv().get("DB_USER");
+        String dbPassword = System.getenv().get("DB_PASSWORD");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(dbUserName);
+        dataSource.setPassword(dbPassword);
+
+        System.out.println("Trying to connect...");
+        dataSource.getConnection().close();
+        System.out.println("Connection OK");
+
+        return dataSource;
+    }
+
+}
