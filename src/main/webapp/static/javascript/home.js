8c0c4c8a0d14be3af04614a3acd7e79c003fa767
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-08 03:10:46 +0200

    remove: delete trial code snippets from home.js

--- src/main/webapp/static/javascript/home.js
+++ src/main/webapp/static/javascript/home.js
@@ -1,33 +1,5 @@
 window.addEventListener('load', async () => {
-    const productCategories = await fetchDataJson("/product-categories");
-    console.log(productCategories);
-
-    const productSuppliers = await fetchDataJson("/product-suppliers");
-    console.log(productSuppliers);
-
-    const productsByCategory = await fetchDataJson("/products-by-category?category-id=2570833b-0fcd-4ebb-8844-9296f2e94259");     // TODO: Change to automatic id search.
-    console.log(productsByCategory);
-
-    const productsBySupplier = await fetchDataJson("/products-by-supplier?supplier-id=02846481-0e34-435e-9c6d-6fd1affde2b9");     // TODO: Change to automatic id search.
-    console.log(productsBySupplier);
-
-    // const addProductToCart = await fetchDataJson("/cart", "POST", {productId: "f7faed42-3519-4b95-9d5d-da0a924cf92f", userId: "bdc7f29e-0aa8-42ca-8070-5baba303185e"});         // TODO: JS Console shows error!
-    // console.log(addProductToCart);
-    //
-    // const removeProductFromCart = await fetchDataJson("/cart", "DELETE", {productId: "f7faed42-3519-4b95-9d5d-da0a924cf92f", userId: "bdc7f29e-0aa8-42ca-8070-5baba303185e"});  // TODO: JS Console shows error!
-    // console.log(addProductToCart);
-
-    const cart = await fetchDataJson("/cart?user-id=bdc7f29e-0aa8-42ca-8070-5baba303185e");
-    console.log(cart);
-
-    const totalPrice = await fetchDataJson("/total-price?user-id=bdc7f29e-0aa8-42ca-8070-5baba303185e");
-    console.log(totalPrice);
-
-    // const addUser = await fetchDataJson("/user", "POST", {email: "proba@proba.hu", password: "titkosJelszo"});  // TODO: JS Console shows error!
-    // console.log(addUser);
-    //
-    // const removeUser = await fetchDataText("/user", "DELETE", "a2e3201d-a825-47fa-9428-959baa7c92e2");  // TODO: JS Console shows error!
-    // console.log(removeUser);
+    console.log("Test log");
 });
 
 async function fetchDataJson(url, methodType, payload){
