b05059d90ffbf3adced29089814f29c8daf350f2
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-16 17:42:35 +0200

    refactor: rename some css class names of registration modal dialog

--- src/main/webapp/static/javascript/registration.js
+++ src/main/webapp/static/javascript/registration.js
@@ -21,9 +21,9 @@ export function registrationButtonAddEventListener() {
                                                                             <div id="registration-modal-title">Register user</div>
                                                                         </div>
                                                                         <div id="registration-modal-content-container">
-                                                                            <input type="text" id="modal-registration-email-input" placeholder="E-mail" required>
-                                                                            <input type="text" id="modal-registration-password-input" placeholder="Password" required>
-                                                                            <input type="text" id="modal-registration-password-confirmation-input" placeholder="Confirm Password" required>
+                                                                            <input type="text" id="registration-modal-email-input" placeholder="E-mail" required>
+                                                                            <input type="text" id="registration-modal-password-input" placeholder="Password" required>
+                                                                            <input type="text" id="registration-modal-password-confirmation-input" placeholder="Confirm Password" required>
                                                                         </div>
                                                                         <div id="registration-modal-footer-container">
                                                                             <div id="registration-modal-cancel-button">Cancel</div>
@@ -52,19 +52,19 @@ function addEventListenerToRegistrationButton() {
     const registrationButton = document.querySelector("#registration-modal-registration-button");
     registrationButton.addEventListener('click', async ()=> {
         // Get input values
-        const modalRegistrationEmailInput = document.querySelector("#modal-registration-email-input");
-        const modalRegistrationEmailInputValue = modalRegistrationEmailInput.value;
+        const registrationModalEmailInput = document.querySelector("#registration-modal-email-input");
+        const registrationModalEmailInputValue = registrationModalEmailInput.value;
 
-        const modalRegistrationPasswordInput = document.querySelector("#modal-registration-password-input");
-        const modalRegistrationPasswordInputValue = modalRegistrationPasswordInput.value;
+        const registrationModalPasswordInput = document.querySelector("#registration-modal-password-input");
+        const registrationModalPasswordInputValue = registrationModalPasswordInput.value;
 
-        const modalRegistrationPasswordConfirmationInput = document.querySelector("#modal-registration-password-confirmation-input");
-        const modalRegistrationPasswordConfirmationInputValue = modalRegistrationPasswordConfirmationInput.value;
+        const registrationModalPasswordConfirmationInput = document.querySelector("#registration-modal-password-confirmation-input");
+        const registrationModalPasswordConfirmationInputValue = registrationModalPasswordConfirmationInput.value;
 
         // Validate input values
-        if (modalRegistrationPasswordInputValue === modalRegistrationPasswordConfirmationInputValue) {
+        if (registrationModalPasswordInputValue === registrationModalPasswordConfirmationInputValue) {
             // Send values to the backend
-            const response = await fetchData("POST", `/user`, null, `{"email": "${modalRegistrationEmailInputValue}", "password": "${modalRegistrationPasswordInputValue}"}`, "application/json", );
+            const response = await fetchData("POST", `/user`, null, `{"email": "${registrationModalEmailInputValue}", "password": "${registrationModalPasswordInputValue}"}`, "application/json", );
 
             // Close modal dialog
             if (response.ok) {
