2390b2dde173605583720399af90e2c49af12223
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-18 23:50:09 +0200

    refactor: rename cart-modal... to checkout-modal...

--- src/main/webapp/static/javascript/main.js
+++ src/main/webapp/static/javascript/main.js
@@ -4,7 +4,7 @@ import {addEventListenerToLoginButton} from "./modules/login.js";
 import {addEventListenerToLogoutButton} from "./modules/logout.js";
 import {setVisibilityOfHeaderButtons} from "./modules/header.js";
 import {addEventListenerToUserButton} from "./modules/user.js";
-import {addEventListenerToCartButton} from "./modules/cart.js";
+import {addEventListenerToCartButton} from "./modules/checkout.js";
 
 // WINDOW ONLOAD //
 window.addEventListener('load', async () => {
