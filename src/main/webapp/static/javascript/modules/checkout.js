fc1d16e673f2155f0f76bcf45a8d01dfebc3fde4
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-19 03:31:06 +0200

    reafctor: form the basic shape of the cart table in checkout modal dialog

--- src/main/webapp/static/javascript/modules/checkout.js
+++ src/main/webapp/static/javascript/modules/checkout.js
@@ -6,7 +6,7 @@ const cartButton = document.querySelector("#cart-button");
 
 // EXPORTED FUNCTIONS //
 export function addEventListenerToCartButton() {
-    cartButton.addEventListener('click', () => {
+    cartButton.addEventListener('click', async () => {
         // Disable scrolling on site
         body.classList.add("block-scroll");
 
@@ -20,22 +20,22 @@ export function addEventListenerToCartButton() {
                                 <div id="checkout-modal-title">Checkout Process</div>
                             </div>
                             <div id="checkout-modal-steps">
-                                <div id="checkout-modal-cart-step" class="checkout-modal-step checkout-modal-step-selected">
+                                <div id="checkout-modal-cart-step" class="checkout-modal-step">
                                     <img src="/static/images/icons/cart.png"><div>Cart</div>
                                 </div>
-                                <img src="/static/images/icons/next.png" class="checkout-model-step-separator">
+                                <img src="/static/images/icons/next.png" class="checkout-modal-step-separator">
                                 <div id="checkout-modal-delivery-step" class="checkout-modal-step">
                                     <img src="/static/images/icons/delivery.png"><div>Delivery</div>
                                 </div>
-                                <img src="/static/images/icons/next.png" class="checkout-model-step-separator">
+                                <img src="/static/images/icons/next.png" class="checkout-modal-step-separator">
                                 <div id="checkout-modal-review-step" class="checkout-modal-step">
                                     <img src="/static/images/icons/review.png"><div>Review</div>
                                 </div>
-                                <img src="/static/images/icons/next.png" class="checkout-model-step-separator">
+                                <img src="/static/images/icons/next.png" class="checkout-modal-step-separator">
                                 <div id="checkout-modal-payment-step" class="checkout-modal-step">
                                     <img src="/static/images/icons/payment.png"><div>Payment</div>
                                 </div>
-                                <img src="/static/images/icons/next.png" class="checkout-model-step-separator">
+                                <img src="/static/images/icons/next.png" class="checkout-modal-step-separator">
                                 <div id="checkout-modal-confirmation-step" class="checkout-modal-step">
                                     <img src="/static/images/icons/confirmation.png"><div>Confirmation</div>
                                 </div>
@@ -59,7 +59,13 @@ export function addEventListenerToCartButton() {
 
         // Add event listener to close button
         addEventListenerToCloseButton();
+
+        // Show cart step
+        await showCartStep();
     });
+
+    // Click when loading site (development only)
+    cartButton.click();
 }
 
 // INNER FUNCTIONS //
@@ -75,4 +81,90 @@ function closeModalDialog() {
     // Remove modal dialog
     const modalDialog = document.querySelector("#modal-dialog");
     modalDialog.remove();
+}
+
+function addEventListenerToNextButton(actionToExecute) {
+    const modalNextButton = document.querySelector("#checkout-modal-next-button");
+    modalNextButton.addEventListener('click', async () => await actionToExecute());
+}
+
+async function showCartStep() {
+    // Highlight cart step
+    const cartStep = document.querySelector("#checkout-modal-cart-step");
+    cartStep.classList.add("checkout-modal-step-selected");
+
+    // Hide previous button
+    const modalPreviousButton = document.querySelector("#checkout-modal-previous-button");
+    modalPreviousButton.classList.add("invisible");
+
+    // Add event listener to next button
+    addEventListenerToNextButton(showDeliveryStep);
+
+    // Empty dialog content
+    const userModalContentContainer = document.querySelector("#checkout-modal-content-container");
+    userModalContentContainer.innerHTML = "";
+
+    // Get cart from backend
+    const cart = await fetchData("GET", `/cart`, {"session-token": sessionStorage.getItem("session-token")}, null, null, "JSON");
+
+    // Show cart content in a table
+    if (cart.length === 0) {
+        userModalContentContainer.insertAdjacentHTML('afterbegin', "<div id='modal-cart-is-empty-text'>Cart is empty, choose product from product categories!</div>");
+    } else {
+        let cartTable = `<table id="checkout-modal-cart-table">
+                            <colgroup>
+                                <col id="checkout-modal-picture-column" >
+                                <col id="checkout-modal-supplier-and-name-column">
+                                <col id="checkout-modal-price-and-currency-column">
+                                <col id="checkout-modal-quantity-column">
+                                <col id="checkout-modal-sub-total-column">
+                                <col id="checkout-modal-buttons-column">
+                            </colgroup>
+                            <tr>
+                                <th class="checkout-modal-left-aligned-column"></th>
+                                <th class="checkout-modal-left-aligned-column"></th>
+                                <th class="checkout-modal-left-aligned-column">Unit price</th>
+                                <th class="checkout-modal-center-aligned-column">Quantity</th>
+                                <th class="checkout-modal-right-aligned-column">Sub-total</th>
+                                <th class="checkout-modal-right-aligned-column"></th>
+                            </tr>`;
+
+        let totalPrice = 0;
+        let currency = cart[0].currency;
+        for (let product of cart) {
+            let subTotalPrice = product.price * product.quantity;
+            totalPrice += subTotalPrice;
+            cartTable += `<tr>
+                            <td class="checkout-modal-left-aligned-column"><img class="checkout-modal-product-photo" src="${product.picture}"></td>
+                            <td class="checkout-modal-left-aligned-column">${product.name}</td>
+                            <td class="checkout-modal-left-aligned-column">${product.price} ${product.currency}</td>
+                            <td class="checkout-modal-center-aligned-column">${product.quantity}</td>
+                            <td class="checkout-modal-right-aligned-column">${subTotalPrice} ${product.currency}</td>
+                            <td class="checkout-modal-right-aligned-column">
+                                <button id="checkout-modal-add-to-cart-button" data-product-id="${product.id}" type="button">+</button> 
+                                <button id="checkout-modal-remove-from-cart-button" data-product-id="${product.id}" type="button">-</button>
+                            </td class="checkout-modal-right-aligned-column">
+                          </tr>`;
+        }
+
+        cartTable += `  <tr>
+                            <td class="checkout-modal-left-aligned-column"></td>
+                            <td class="checkout-modal-left-aligned-column"></td>
+                            <td class="checkout-modal-left-aligned-column"></td>
+                            <td class="checkout-modal-center-aligned-column" id="checkout-modal-total-prize-text">Total:</td>
+                            <td class="checkout-modal-right-aligned-column" id="checkout-modal-total-price">${totalPrice} ${currency}</td>
+                            <td class="checkout-modal-right-aligned-column"></td>
+                        </tr>
+                      </table>`;
+
+        userModalContentContainer.insertAdjacentHTML('afterbegin', cartTable);
+    }
+
+
+
+
+}
+
+async function showDeliveryStep() {
+    console.log("Delivery Step Content");
 }
