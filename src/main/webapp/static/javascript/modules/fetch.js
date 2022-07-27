be5d2612c1dcc857772c4c253e04bb4701fcb2f7
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-07-27 11:18:31 +0200

    feat: add scrolling to product container

--- src/main/webapp/static/javascript/modules/fetch.js
+++ src/main/webapp/static/javascript/modules/fetch.js
@@ -1,16 +1,16 @@
 // EXPORTED FUNCTION(S) //
-export async function fetchData(methodType, url, headerContent, bodyContent, contentType, expectedType) {
+export async function fetchData(methodType, url, headerContent, bodyContent, contentType, responseType) {
     let request;
-    if (headerContent != null || bodyContent != null || contentType!= null || expectedType != null) {
+    if (headerContent != null || bodyContent != null || contentType!= null || responseType != null) {
         request = { method: methodType,
             headers: { 'Content-type': contentType, ...headerContent },
             body: bodyContent };
     }
 
     const response = await fetch(url, request);
-    if (expectedType == "JSON" && response.ok) {
+    if (responseType == "JSON" && response.ok) {
         return await response.json();
-    } else if (expectedType == "Text" && response.ok) {
+    } else if (responseType == "Text" && response.ok) {
         return await response.text();
     } else {
         return response;
