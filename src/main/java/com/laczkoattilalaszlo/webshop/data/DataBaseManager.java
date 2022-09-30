b72c7cb05bcc57ec586557c89b16cc1a4cee9c0e
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-09-30 23:00:41 +0200

    refactor: prepare DataSource both for local and  production use

--- src/main/java/com/laczkoattilalaszlo/webshop/data/DataBaseManager.java
+++ src/main/java/com/laczkoattilalaszlo/webshop/data/DataBaseManager.java
@@ -15,14 +15,17 @@ public class DataBaseManager {
     public DataSource connect() throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
-//        String dbName = System.getenv().get("DB_NAME");
-//        String dbUserName = System.getenv().get("DB_USER");
-//        String dbPassword = System.getenv().get("DB_PASSWORD");
-//
-//        dataSource.setDatabaseName(dbName);
-//        dataSource.setUser(dbUserName);
-//        dataSource.setPassword(dbPassword);
-        dataSource.setURL(System.getenv().get("JDBC_DATABASE_URL"));
+        if (System.getenv().get("JDBC_DATABASE_URL") == null) {
+            String dbName = System.getenv().get("DB_NAME");
+            String dbUserName = System.getenv().get("DB_USER");
+            String dbPassword = System.getenv().get("DB_PASSWORD");
+
+            dataSource.setDatabaseName(dbName);
+            dataSource.setUser(dbUserName);
+            dataSource.setPassword(dbPassword);
+        } else {
+            dataSource.setURL(System.getenv().get("JDBC_DATABASE_URL"));
+        }
 
         System.out.println("Trying to connect...");
         dataSource.getConnection().close();
