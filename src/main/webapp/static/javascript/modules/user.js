97fe7e78e624c9d8f1c5a5eb57586430e83091c5
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-10-03 01:45:46 +0200

    heroku: modify JavaScript fetch character coding

--- src/main/webapp/static/javascript/modules/user.js
+++ src/main/webapp/static/javascript/modules/user.js
@@ -197,7 +197,7 @@ function addEventListenerToShippingAddressTab() {
 
             // Update user data
             console.log(userModalNameInputValue);
-            const response = await fetchData("PUT", `/address?type=shipping`, {"session-token": sessionStorage.getItem("session-token")}, `{"name": "${userModalNameInputValue}", "zip": "${userModalZipInputValue}", "country": "${userModalCountryInputValue}", "city": "${userModalCityInputValue}", "address": "${userModalAddressInputValue}"}`, "application/json", null);
+            const response = await fetchData("PUT", `/address?type=shipping`, {"session-token": sessionStorage.getItem("session-token")}, `{"name": "${userModalNameInputValue}", "zip": "${userModalZipInputValue}", "country": "${userModalCountryInputValue}", "city": "${userModalCityInputValue}", "address": "${userModalAddressInputValue}"}`, "application/json; charset=utf-8", null);
 
             // Show result of update operation in modal dialog
             const UserModalOperationResult = document.querySelector("#user-modal-operation-result");
