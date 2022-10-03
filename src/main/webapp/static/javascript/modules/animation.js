8d06099dc85eebd9608a1706f53c5d78245681ff
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-10-03 21:57:50 +0200

    fix: product cards are slipping under the search bar

--- src/main/webapp/static/javascript/modules/animation.js
+++ src/main/webapp/static/javascript/modules/animation.js
@@ -1,9 +1,19 @@
 // EXPORTED FUNCTION(S) //
 export function fadeInElements(classNameOfElementsToFade, duration) {
     let elements = document.getElementsByClassName(classNameOfElementsToFade);
+
+    // Fade in elements
     for (let i = 0; i < elements.length; ++i) {
         setTimeout(() => {
             elements[i].classList.add('animation-fadein');
+        }, i * duration);
+    }
+
+    // Remove fade-in related classes from elements
+    for (let i = 0; i < elements.length; ++i) {
+        setTimeout(() => {
+            elements[i].classList.remove('animated');
+            elements[i].classList.remove('animation-fadein');
         }, i * duration);
     }
 }
